552e0000b36c7abfcf6334ff34d51bff243dbc30
ren5602 <naziz5602@gmail.com>
2023-11-23 16:16:12 +0700

    pb 1

---
+++ Terimakasih_19.java
@@ -0,0 +1,15 @@
+public class Terimakasih_19{
+    // fungsi UcapanTerimaKasih 
+    public static void UcapanTerimaKasih(){
+        // isi dari fungsi UcapanTerimaKasih
+        System.out.println("Thank you being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything");
+    }   
+    public static void main(String[] args){
+        System.out.println("Thank you being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything");
+        // pemanggilan fungsi UcapanTerimaKasih ke dalam void main
+        UcapanTerimaKasih();
+    }
+
+}
